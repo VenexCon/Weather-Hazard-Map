6afb49187cef63c8a366293291b334b9efa200ca
VenexCon <venexconsultant@outlook.com>
2022-09-05 00:02:41 +0100

    <bug> if check required on HazardEL,

--- src/app/dom.js
+++ src/app/dom.js
@@ -66,7 +66,7 @@ const appendNewHazard = (element, string) => {
   hazardChoice.appendChild(tooltip);
 };
 
-const hazardChoices = (data) => {
+const hazardChoices = (data) => { // not currently used
   const average = document.querySelector("#averageBox");
   const high = document.querySelector(".data-high");
   const low = document.querySelector(".data-low");
@@ -74,20 +74,14 @@ const hazardChoices = (data) => {
   const humidity = document.querySelector(".data-humid");
 };
 
-/* const createToolTip = (string, element) => {
-  const tooltip = document.createElement("div");
-  tooltip.setAttribute("class", "tooltip");
 
-  const tooltipText = document.createElement("div");
-  tooltipText.setAttribute("class", "tooltip-text");
-  tooltipText.innerText = `${string}`;
-};
- */
 const isHot = (value) => {
   const coldString =
-    "cold temperatures. Ensure you check weather conditions for driving and tyre depth.";
+    `Cold temperatures. Ensure you check weather conditions for driving, ensure tyre depth is more than 10mm. It is recommended to pack additional clothing and 
+    warm drinks, prior to setting off.`;
   const hotString =
-    "Warm Temperatures. Ensure you have an adequate amounts of water and sunblock for site working, heat stroke is a risk when working in warmer weather.";
+    `Warm Temperatures. Ensure you have an adequate amounts of water and sunblock for site working, heat stroke is a risk when working in warmer 
+    weather.Adequate breaks should be taken throughout the day, ensuring that all employees look after eachother.`;
   if (value > 20) {
     const highImg = document.createElement("i");
     highImg.setAttribute("class", "fa-solid fa-temperature-arrow-up");
@@ -102,27 +96,34 @@ const isHot = (value) => {
 isHot(34);
 
 const isSunny = (value, time) => {
-  if (value < 50 && time > 6 && time < 21) {
+  const sunString = `Sunny weather presents a risk of UV radiation, which damages the skin and can cause burns, blisters, with chronic exposure leading to skin cancer
+                      it is advised to wear long sleeves, apply sunblock factor(30 minimum) and to reapply every two hours. Tinted safety glasses should be worn.`
+  if (value < 50) {
     const sunny = document.createElement("i");
     sunny.setAttribute("class", "fa-solid fa-sun");
-    appendNewHazard(sunny);
+    appendNewHazard(sunny, sunString);
   }
 };
 
 const isWindy = (value) => {
+  const windString = `Wind conditions are currently above 20m per second, this presents a hazard when using access equipment and working at height. 
+  You should review all planned lifts, site works and working practices to identify control measures for newly present hazards. `
   if (value > 20) {
     const windIcon = document.createElement("i");
     windIcon.setAttribute("class", "fa-solid fa-wind");
-    appendNewHazard(windIcon);
+    appendNewHazard(windIcon, windString);
   }
   return;
 };
 
 const isSnowing = (string) => {
+const snowString = `Conditions are currently snowy, and as such all long journeys, and large plant movements should be reviewed in light of the new hazards.
+It is recommended to not travel wear possible, and to ensure that where travel is required, you pack additional warm clothing, a hot drink, and food. Be aware, breakdown
+recovery times can be longer, or you may not be recovered at all during snowy conditions. `
   if (string.includes("snow")) {
     const snowIcon = document.createElement("i");
     snowIcon.setAttribute("class", "fa-solid fa-snowflake");
-    appendNewHazard(snowIcon);
+    appendNewHazard(snowIcon, snowString);
   }
 };
 
@@ -147,6 +148,7 @@ const isThunder = (string) => {
 
 const domAppend = (data) => {
   const hour = getHours(data.dt);
+  console.log(hour)
   const degree = `\u00B0C`;
   const farenheight = `\u00B0`;
   const dataIcon = `${data.weather[0].icon}`;

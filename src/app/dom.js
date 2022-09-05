f7ec034275995afb02aad442ddaaa33bf8fea71b
VenexCon <venexconsultant@outlook.com>
2022-09-05 21:02:22 +0100

    <update> Weather funcs completed, create list of extras

--- src/app/dom.js
+++ src/app/dom.js
@@ -66,7 +66,8 @@ const appendNewHazard = (element, string) => {
   hazardChoice.appendChild(tooltip);
 };
 
-const hazardChoices = (data) => { // not currently used
+const hazardChoices = (data) => {
+  // not currently used
   const average = document.querySelector("#averageBox");
   const high = document.querySelector(".data-high");
   const low = document.querySelector(".data-low");
@@ -74,13 +75,10 @@ const hazardChoices = (data) => { // not currently used
   const humidity = document.querySelector(".data-humid");
 };
 
-
 const isHot = (value) => {
-  const coldString =
-    `Cold temperatures. Ensure you check weather conditions for driving, ensure tyre depth is more than 10mm. It is recommended to pack additional clothing and 
+  const coldString = `Cold temperatures. Ensure you check weather conditions for driving, ensure tyre depth is more than 10mm. It is recommended to pack additional clothing and 
     warm drinks, prior to setting off.`;
-  const hotString =
-    `Warm Temperatures. Ensure you have an adequate amounts of water and sunblock for site working, heat stroke is a risk when working in warmer 
+  const hotString = `Warm Temperatures. Ensure you have an adequate amounts of water and sunblock for site working, heat stroke is a risk when working in warmer 
     weather.Adequate breaks should be taken throughout the day, ensuring that all employees look after eachother.`;
   if (value > 20) {
     const highImg = document.createElement("i");
@@ -93,11 +91,9 @@ const isHot = (value) => {
   }
 };
 
-isHot(34);
-
 const isSunny = (value, time) => {
   const sunString = `Sunny weather presents a risk of UV radiation, which damages the skin and can cause burns, blisters, with chronic exposure leading to skin cancer
-                      it is advised to wear long sleeves, apply sunblock factor(30 minimum) and to reapply every two hours. Tinted safety glasses should be worn.`
+                      it is advised to wear long sleeves, apply sunblock factor(30 minimum) and to reapply every two hours. Tinted safety glasses should be worn.`;
   if (value < 50) {
     const sunny = document.createElement("i");
     sunny.setAttribute("class", "fa-solid fa-sun");
@@ -107,7 +103,7 @@ const isSunny = (value, time) => {
 
 const isWindy = (value) => {
   const windString = `Wind conditions are currently above 20m per second, this presents a hazard when using access equipment and working at height. 
-  You should review all planned lifts, site works and working practices to identify control measures for newly present hazards. `
+  You should review all planned lifts, site works and working practices to identify control measures for newly present hazards. `;
   if (value > 20) {
     const windIcon = document.createElement("i");
     windIcon.setAttribute("class", "fa-solid fa-wind");
@@ -117,9 +113,9 @@ const isWindy = (value) => {
 };
 
 const isSnowing = (string) => {
-const snowString = `Conditions are currently snowy, and as such all long journeys, and large plant movements should be reviewed in light of the new hazards.
+  const snowString = `Conditions are currently snowy, and as such all long journeys, and large plant movements should be reviewed in light of the new hazards.
 It is recommended to not travel wear possible, and to ensure that where travel is required, you pack additional warm clothing, a hot drink, and food. Be aware, breakdown
-recovery times can be longer, or you may not be recovered at all during snowy conditions. `
+recovery times can be longer, or you may not be recovered at all during snowy conditions. `;
   if (string.includes("snow")) {
     const snowIcon = document.createElement("i");
     snowIcon.setAttribute("class", "fa-solid fa-snowflake");
@@ -128,17 +124,21 @@ recovery times can be longer, or you may not be recovered at all during snowy co
 };
 
 const isLowVisibility = (value) => {
+  const lowString = `Visibility is under 100m, You should exercise caution if travel is required.
+   Re-arrange tarvel plans where possible.`;
   if (value < 100) {
     const lowVisIcon = document.createElement("i");
     lowVisIcon.setAttribute("class", "fa-solid fa-eye-slash");
-    appendNewHazard(lowVisIcon);
+    appendNewHazard(lowVisIcon, lowString);
   }
 };
 
 const isThunder = (string) => {
+  const thunderString = `Storms are forecast or present, working at height should be avoided due to risk of lightening strikes`;
   if (string.includes("storm")) {
-    thunderIcon = document.createElement("i");
+    const thunderIcon = document.createElement("i");
     thunderIcon.setAttribute("class", "fa-solid fa-cloud-bolt");
+    appendNewHazard(thunderIcon, thunderString);
   }
 };
 
@@ -148,7 +148,7 @@ const isThunder = (string) => {
 
 const domAppend = (data) => {
   const hour = getHours(data.dt);
-  console.log(hour)
+  console.log(hour);
   const degree = `\u00B0C`;
   const farenheight = `\u00B0`;
   const dataIcon = `${data.weather[0].icon}`;

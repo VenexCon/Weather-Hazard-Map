8a0d34bac47b5a0e888888ba676d83e564ca3e7c
VenexCon <venexconsultant@outlook.com>
2022-10-02 23:01:36 +0100

    <update> updated api.js with forecast fetch, resolve promise required

--- src/app/api.js
+++ src/app/api.js
@@ -10,6 +10,7 @@ const keyStorage = (() => {
   return { viewKey };
 })();
 
+//Daily Forecast
 const fetchWeather = async (location) => {
   try {
     const response = await fetch(
@@ -18,10 +19,25 @@ const fetchWeather = async (location) => {
     ); // fetch returns a promise
     let data = await response.json(); //.json() returns a promise.
     console.log(data);
+    const forecast = fetchFiveForecast(data.coord.lat, data.coord.lon);
     return domAppend(data); // if you try and return data, it is called prior to awaiting
   } catch (error) {
     console.log(error);
     alert(`Location Not Found`);
+  }
+};
+
+const fetchFiveForecast = async (lat, lon) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${keyStorage.viewKey()}`,
+      { mode: "cors" }
+    );
+    const data = response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -34,11 +50,7 @@ const getInput = (e) => {
 const form = document.querySelector(".search-container");
 form.addEventListener("submit", (e) => {
   fetchWeather(getInput(e));
+  fetchFiveForecast(getInput(e));
 });
-
-//do i need ot return it here? the function needs to be called
-//pass that to a object, that can be destructured.
-//this can all happen in the EL.
-//need a function that takes the object resposne, destructure it, and then assign it to the dom Object.
 
 export { fetchWeather };
